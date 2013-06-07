e54b99b21da1a7513cf77f81dba714a8ced30b4e
Uwe Kamper <me@uwekamper.de>
2013-06-07 18:31:23 +0200

    Project initialization

---
+++ cort_n_ctore/cort_n_ctore/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'cort_n_ctore.views.home', name='home'),
+    # url(r'^cort_n_ctore/', include('cort_n_ctore.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
